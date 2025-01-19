182c2d5fc84417f979687741d99bc656b86777cf
brunorwanda4 <brunorwanda4@gmail.com>
2025-01-20 00:56:20 +0200

    c

--- src/components/site/collection/users/UserPageCollection.tsx
+++ src/components/site/collection/users/UserPageCollection.tsx
@@ -15,24 +15,32 @@ interface props {
   collectionName: string;
 }
 
-
-
 const UserPageCollection = async ({ collectionName }: props) => {
   const data: UserModel[] | FetchError = await apiClient.allData<UserModel[]>(
     "users",
     "users"
   );
 
-  const userRoles : UserRoleModel[] | FetchError = await fetchUserRole();
+  const userRoles: UserRoleModel[] | FetchError = await fetchUserRole();
 
   if (Array.isArray(data)) {
     return (
-      <CollectionPageStatic className=" overflow-x-hidden" collection={collectionName}>
+      <CollectionPageStatic
+        className=" overflow-x-hidden"
+        collection={collectionName}
+      >
         <div className="min-h-48 flex gap-4 justify-between">
-          <UserCollectionDetails totalUserRole={Array.isArray(userRoles) && userRoles.length} totalUser={data.length} />
+          <UserCollectionDetails
+            totalUserRole={Array.isArray(userRoles) && userRoles.length}
+            totalUser={data.length}
+          />
           {Array.isArray(userRoles) && <CollectionUserRole roles={userRoles} />}
         </div>
-         <UsersTableCollection collectionName={collectionName} usersRole={userRoles} users={data}/>
+        <UsersTableCollection
+          collectionName={collectionName}
+          usersRole={userRoles}
+          users={data}
+        />
       </CollectionPageStatic>
     );
   }
@@ -40,4 +48,3 @@ const UserPageCollection = async ({ collectionName }: props) => {
   return <CollectionPageErrorStatic error={data} collection={collectionName} />;
 };
 export default UserPageCollection;
-
